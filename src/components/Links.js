8931de100815b5766c5603ab8fb46f65732e61ff
florence <loracodes3@gmail.com>
2024-11-10 04:10:43 +0300

    commit

--- src/components/Links.js
+++ src/components/Links.js
@@ -2,15 +2,11 @@ import React from "react";
 
 function Links({ github, linkedin }) {
   return (
-    <div>
+    <div id="links">
       <h3>Links</h3>
-      <a href={github} target="_blank" rel="noopener noreferrer">
-        {github}
-      </a>
+      <a href={github} target="_blank" rel="noopener noreferrer">{github}</a>
       <br />
-      <a href={linkedin} target="_blank" rel="noopener noreferrer">
-        {linkedin}
-      </a>
+      <a href={linkedin} target="_blank" rel="noopener noreferrer">{linkedin}</a>
     </div>
   );
 }
