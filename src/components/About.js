3645b7c8ac249523fb6fe6a608e95b7c5f74ebf3
florence <loracodes3@gmail.com>
2024-11-10 04:09:59 +0300

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,11 @@
 import React from "react";
-import Links from "./Links"; // Import the Links component
 
-function About({ bio, github, linkedin }) {
+function About({ bio }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {/* Conditional rendering for bio */}
-      {bio && bio.trim() !== "" && <p>{bio}</p>} {/* Only render bio if it is provided and not empty */}
-      
-      {/* Pass the github and linkedin props to Links component */}
-      <Links github={github} linkedin={linkedin} />
+      {/* Conditional rendering of bio */}
+      {bio && <p>{bio}</p>}
     </div>
   );
 }
