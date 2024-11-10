9c625e003b0940ff167426b136e332cfe8810694
florence <loracodes3@gmail.com>
2024-11-10 03:53:33 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
-
 function App() {
+  // Define all variables here
+  const username = "Liz";
+  const city = "New York";
+  const bio = "I am a passionate web developer who loves creating impactful websites.";
+  const github = "https://github.com/liz";
+  const linkedin = "https://linkedin.com/in/liz";
+
   return (
     <div>
-      <NavBar />
-      <Home username ={username} city ={city}/>
-      <About bio ={bio} github={github} linkedin={linkedin}/>
+      <Home username={username} city={city} />
+      <About bio={bio} github={github} linkedin={linkedin} />
     </div>
   );
 }
