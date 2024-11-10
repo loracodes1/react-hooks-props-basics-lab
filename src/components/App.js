8f36cbfbfc88d20aa41cd31748a7b2296a25f6b3
florence <loracodes3@gmail.com>
2024-11-10 03:45:31 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
       <NavBar />
       <Home username ={username} city ={city}/>
-      <About />
+      <About bio ={bio} github={github} linkedin={linkedin}/>
     </div>
   );
 }
